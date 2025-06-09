fd66c0af9076efb364d218fea906c9d9b10aaf3a
Ameyabarve123 <barveameya@yahoo.com>
2025-06-09 14:03:30 -0400

    File structure and memory cap
    I created the file structure for this project. I also researched into how to put a memory limit to the program.

---
+++ calculator/perceptron.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Perceptron Calculator Started." << endl;
+
+  // test to see if 
+  int* arr = new int[20 * 1024 * 1024]; 
+  arr[0] = 42;
+  delete[] arr;
+  return 0;
+}
